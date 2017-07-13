6ad7404b814e447048fd6713793eed8e0bbbd413
Jack Grigg <jack@z.cash>
2017-07-13 15:10:13 -0500

    Update print-soln.py to parse current blocks
    Closes #8.

--- print-soln.py
+++ print-soln.py
@@ -4,6 +4,7 @@ from binascii import unhexlify
 from pyblake2 import blake2b
 import struct
 
+from convert import expand_array
 from pow import (
     hash_xi,
     print_hash,
@@ -30,14 +31,39 @@ class node(object):
             ret += child.__repr__(level+[False])
         return ret
 
+def get_indices_from_minimal(minimal, bit_len):
+    eh_index_size = 4
+    assert (bit_len+7)/8 <= eh_index_size
+    len_indices = 8*eh_index_size*len(minimal)/bit_len
+    byte_pad = eh_index_size - (bit_len+7)/8
+    expanded = expand_array(minimal, len_indices, bit_len, byte_pad)
+    return [struct.unpack('>I', expanded[i:i+4])[0] for i in range(0, len_indices, eh_index_size)]
+
 def generate_hashes(n, k, header):
-    digest = blake2b(digest_size=n/8, person=zcash_person(n, k))
+    collision_length = n/(k+1)
+    bit_len = collision_length + 1
+    hash_length = (k+1)*((collision_length+7)//8)
+    indices_per_hash_output = 512/n
+    num_indices = 2**k
+
+    digest = blake2b(digest_size=(512/n)*n/8, person=zcash_person(n, k))
     digest.update(header[:140])
-    numIndices = ord(header[140]) if ord(header[140]) < 256 else struct.unpack('<H', header[141:143])
-    assert numIndices == 2**k, 'Block header does not match Equihash parameters'
-    i = 143 if ord(header[140]) == 256 else 141
-    soln = [struct.unpack('<I', header[i:i+4])[0] for i in range(i, i+numIndices*4, 4)]
-    hashes = [hash_xi(digest.copy(), xi).digest() for xi in soln]
+    num_bytes = ord(header[140]) if ord(header[140]) < 253 else struct.unpack('<H', header[141:143])[0]
+    assert num_bytes == bit_len*num_indices/8, 'Block header does not match Equihash parameters'
+    i = 143 if ord(header[140]) == 253 else 141
+    soln = get_indices_from_minimal(bytearray(header[i:i+num_bytes]), bit_len)
+
+    hashes = []
+    for xi in soln:
+        r = xi % indices_per_hash_output
+        # X_i = H(I||V||x_i)
+        curr_digest = digest.copy()
+        hash_xi(curr_digest, xi/indices_per_hash_output)
+        tmp_hash = curr_digest.digest()
+        hashes.append(
+            expand_array(bytearray(tmp_hash[r*n/8:(r+1)*n/8]),
+                         hash_length, collision_length)
+        )
     return soln, hashes
 
 def print_hashes(soln, hashes):
